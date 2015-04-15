ec5b944e99dc356ec7fd29bfe9ec7e67fb42d400
arisvang <alexanderkurerisvang@gmail.com>
2015-04-15 14:50:30 +0200

    First tutorial DONE

--- app/src/main/java/com/oose/alexanderrisvang/katrinehej/MainActivity.java
+++ app/src/main/java/com/oose/alexanderrisvang/katrinehej/MainActivity.java
@@ -2,8 +2,11 @@ package com.oose.alexanderrisvang.katrinehej;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
 
 
@@ -23,6 +26,24 @@ public class MainActivity extends ActionBarActivity {
         phoneTxt = (EditText) findViewById(R.id.txtPhone);
         emailTxt = (EditText) findViewById(R.id.txtEmail);
         addressTxt = (EditText) findViewById(R.id.txtAddress);
+        final Button addbtn = (Button) findViewById(R.id.btnAdd);
+
+        nameTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                addbtn.setEnabled(s.equals(""));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     @Override
