57d301d66b63e34f7cad6d345ef54abb60b98687
arisvang <alexanderkurerisvang@gmail.com>
2015-04-15 12:30:29 +0200

    PUSH TWO

--- app/src/main/java/com/oose/alexanderrisvang/katrinehej/MainActivity.java
+++ app/src/main/java/com/oose/alexanderrisvang/katrinehej/MainActivity.java
@@ -12,8 +12,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Hej allesammen hva så?
     }
-// Hej
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
